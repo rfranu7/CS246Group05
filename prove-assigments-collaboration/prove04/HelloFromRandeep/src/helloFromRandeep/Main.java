5d8607c5c42e46751cdfa883bb86fa353ac2d6ef
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2020-10-06 11:12:44 +0800

    Added prove04 and HelloFromRandeep folders

---
+++ prove-assigments-collaboration/prove04/HelloFromRandeep/src/helloFromRandeep/Main.java
@@ -0,0 +1,11 @@
+package helloFromRandeep;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Randeep";
+        System.out.format("Hello, my name is %s", name);
+
+    }
+}
