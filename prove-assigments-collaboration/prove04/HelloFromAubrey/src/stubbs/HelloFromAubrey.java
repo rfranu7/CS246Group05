796db0649772a32c72b8e3235391d23acc5c1fd1
browner86 <browner86@gmail.com>
2020-10-08 18:14:18 -0400

    Added response to Hello.

--- prove-assigments-collaboration/prove04/HelloFromAubrey/src/stubbs/HelloFromAubrey.java
+++ prove-assigments-collaboration/prove04/HelloFromAubrey/src/stubbs/HelloFromAubrey.java
@@ -4,5 +4,7 @@ public class HelloFromAubrey {
 
     public static void main(String[] args) {
         System.out.print("Hello, my name is Aubrey");
+        System.out.println("\nHello, Aubrey My name is Brian");
+        System.out.println("This is a very complicated way to chat.");
     }
 }
