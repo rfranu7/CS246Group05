154bda24ce5d241efbe9d465cf2b8fefdfff8117
rfranu7 <57077484+rfranu7@users.noreply.github.com>
2020-10-10 07:25:45 +0800

    Replied Hi

--- prove-assigments-collaboration/prove04/taelor_says_hi/src/prove04/taelorSaysHi.java
+++ prove-assigments-collaboration/prove04/taelor_says_hi/src/prove04/taelorSaysHi.java
@@ -3,6 +3,8 @@ package prove04;
 public class taelorSaysHi {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Taelor.");
+
+        System.out.println("Hello, my name is Taelor.");
+        System.out.println("Hello Taelor, My name is Randeep.");
     }
 }
