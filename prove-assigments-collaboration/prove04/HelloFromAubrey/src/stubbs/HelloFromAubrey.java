6af347e0dfd43f1ac7a7095530b9779d28a80465
aaubreyaann <64035581+aaubreyaann@users.noreply.github.com>
2020-10-08 17:45:53 -0700

    Modified for stretch challenge

--- prove-assigments-collaboration/prove04/HelloFromAubrey/src/stubbs/HelloFromAubrey.java
+++ prove-assigments-collaboration/prove04/HelloFromAubrey/src/stubbs/HelloFromAubrey.java
@@ -6,5 +6,6 @@ public class HelloFromAubrey {
         System.out.print("Hello, my name is Aubrey");
         System.out.println("\nHello, Aubrey My name is Brian");
         System.out.println("This is a very complicated way to chat.");
+        System.out.println("I agree");
     }
 }
