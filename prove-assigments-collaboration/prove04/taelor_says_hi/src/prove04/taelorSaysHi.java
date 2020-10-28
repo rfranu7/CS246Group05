25663009c22d24b8797dfbd00d4fd043f1d70e53
Aubrey <64035581+aaubreyaann@users.noreply.github.com>
2020-10-28 09:50:55 -0700

    Test by Aubrey

--- prove-assigments-collaboration/prove04/taelor_says_hi/src/prove04/taelorSaysHi.java
+++ prove-assigments-collaboration/prove04/taelor_says_hi/src/prove04/taelorSaysHi.java
@@ -6,7 +6,7 @@ public class taelorSaysHi {
 
         System.out.println("Hello, my name is Taelor.");
         System.out.println("Hello Taelor, My name is Randeep.");
-        // this is the recent change
+        // this is the recent change by Aubrey
         System.out.println("This is week 07!");
     }
 }
