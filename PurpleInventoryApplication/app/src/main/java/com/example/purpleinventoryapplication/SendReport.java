c651a174561b90ece1c1d063a273036d1e3dd8cc
browner86 <browner86@gmail.com>
2020-12-09 20:55:50 -0500

    Add email functionality create csv for data selected. attach csv to email. something is wrong with permissions still.

--- PurpleInventoryApplication/app/src/main/java/com/example/purpleinventoryapplication/SendReport.java
+++ PurpleInventoryApplication/app/src/main/java/com/example/purpleinventoryapplication/SendReport.java
@@ -1,15 +1,24 @@
 package com.example.purpleinventoryapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author Team-05
@@ -35,21 +44,74 @@ public class SendReport extends AppCompatActivity {
     public void sendReport(View view) {
         // After button is clicked, save the last email used
         EditText emailField = (EditText) findViewById(R.id.reportEmail);
-        EditText startDateField = (EditText) findViewById(R.id.start_date);
-        EditText endDateField = (EditText) findViewById(R.id.end_date);
+        final EditText startDateField = (EditText) findViewById(R.id.start_date);
+        final EditText endDateField = (EditText) findViewById(R.id.end_date);
 
         Log.d("SEND REPORT", "REPORT STARTED");
 
-        String email = emailField.getText().toString();
+        final String email = emailField.getText().toString();
         Date startDate = new Date(startDateField.getText().toString());
         Date endDate = new Date(endDateField.getText().toString());
 
         Report report = new Report(this);
-        report.getReportByDate(startDate, endDate);
+        report.getReportByDate(startDate, endDate, new VolleyStringObject() {
+            @Override
+            public void onGetDataByIdSuccess(Map<String, Object> data) {
+                String startDate = startDateField.getText().toString() ;
+                String endDate = endDateField.getText().toString();
+                export(data, startDate, endDate, email);
+            }
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("EMAIL", email);
         editor.apply();
+        
+
+
+    }
+
+    public void export(Map<String,Object> data,String startDateField, String endDateField, String email){
+        //generate data
+//        StringBuilder data = new StringBuilder();
+//        data.append("Time,Distance");
+//        for(int i = 0; i<5; i++){
+//            data.append("\n"+String.valueOf(i)+","+String.valueOf(i*i));
+//        }
+
+        try{
+            //saving the file into device
+            FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
+            out.write((data.toString()).getBytes());
+            out.close();
+
+            //exporting
+            Context context = this.getApplicationContext();
+            File filelocation = new File(getFilesDir(), "data.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.purpleinventoryapplication.fileprovider", filelocation);
+            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+            fileIntent.setType("text/csv");
+            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+            String subject = "Report: " + startDateField + " - " + endDateField;
+            String message = "Attached is the report for " + startDateField + " - " + endDateField;
+
+            fileIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{ email});
+            fileIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            fileIntent.putExtra(Intent.EXTRA_TEXT, message);
+            List<ResolveInfo> resInfoList = context.getPackageManager().queryIntentActivities(fileIntent, PackageManager.MATCH_DEFAULT_ONLY);
+            for (ResolveInfo resolveInfo : resInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                context.grantUriPermission(packageName, path, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+
+            startActivity(Intent.createChooser(fileIntent, "Send mail"));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
---
+++ PurpleInventoryApplication/app/src/main/java/com/example/purpleinventoryapplication/VolleyStringObject.java
@@ -0,0 +1,7 @@
+package com.example.purpleinventoryapplication;
+
+import java.util.Map;
+
+public interface VolleyStringObject {
+    public void onGetDataByIdSuccess(Map<String, Object> data);
+}
