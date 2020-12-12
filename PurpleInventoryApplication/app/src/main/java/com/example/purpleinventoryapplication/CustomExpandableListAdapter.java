c6a5e55e8068818e2b5f5dd971ae57865df08ac2
Aubrey <64035581+aaubreyaann@users.noreply.github.com>
2020-12-11 20:17:31 -0800

    Merge conflict ressolution

--- PurpleInventoryApplication/app/src/main/java/com/example/purpleinventoryapplication/CustomExpandableListAdapter.java
+++ PurpleInventoryApplication/app/src/main/java/com/example/purpleinventoryapplication/CustomExpandableListAdapter.java
@@ -6,6 +6,7 @@ import java.util.TreeMap;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
